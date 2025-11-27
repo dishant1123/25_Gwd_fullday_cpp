83760ddf6911bd26fe3e9d2551ad38dcf32f6020
dishant1123 <dishantshah1417@gmail.com>
2025-11-27 09:56:17 +0530

    ...

---
+++ 27nov.cpp
@@ -0,0 +1,32 @@
+// heelo ram . 
+/*
+#include<iostream>
+int main()
+{
+    std :: cout<<"heellooo";
+    return 0; 
+}
+*/ 
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"ram";
+    return 0;
+}
+    */ 
+
+/*
+1.start, digit  --> special character ,  @a =10 ,12a =10 
+2. correct way ==> int a12 =90 ,int a_ =89
+
+*/  
+#include<iostream>
+using namespace std;
+int main()
+{
+    int _=10;
+    cout<<"A is "<<_;
+    return 0; 
+}
