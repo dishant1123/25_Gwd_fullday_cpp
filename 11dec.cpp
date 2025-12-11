aced068276cb81c6676ed73ccc70c333c8fc6197
dishant1123 <dishantshah1417@gmail.com>
2025-12-11 09:29:02 +0530

    ...

---
+++ 11dec.cpp
@@ -0,0 +1,38 @@
+/*
+private  :access within  class only.  
+
+*/
+/*
+#include <iostream>
+using namespace std; 
+class student
+{
+    private :
+        string name="saumya"; 
+        int marks =99; 
+    
+        void show()
+        {
+            cout<<"name :"<<name<<"\n";
+            cout<<"marks :"<<marks<<endl;
+        }
+    public : 
+        void display()
+        {
+            show();
+        }
+
+};
+int main()
+{
+    student s1;  
+    // cout<<"name :"<<s1.name<<"\n";
+    // cout<<"marks :"<<s1.marks<<endl; // bcz  of private  not access outside the class .
+    s1.display();
+    return 0; 
+}
+*/ 
+
+// ex :2 
+
+
