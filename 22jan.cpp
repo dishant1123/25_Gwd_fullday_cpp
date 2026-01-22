761d306d437eeb85157e03e3c55be61c9563f5e3
dishant1123 <dishantshah1417@gmail.com>
2026-01-22 09:30:28 +0530

    ....

---
+++ 22jan.cpp
@@ -0,0 +1,14 @@
+/*
+bank  : 
+    1. pin generate 
+    2. encapsulation  :
+        balance ==> private  
+    3. inheritance    ===> class saving , class current   
+    4. using friend  function  ==> calculation  interest 
+        menu  driven  : 
+            1. enter the pin  for  generating account 
+            2. current  ==> enter pin  ==> deposit  withdraw checkbalance    
+            3. saving   ==> enter  pin ==> deposit  withdraw checkbalance
+            4. exit 
+
+*/
