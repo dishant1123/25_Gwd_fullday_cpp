64e4f6a8102e4ec0a81bca55045ded6fadc3731f
dishant1123 <dishantshah1417@gmail.com>
2026-02-17 12:51:34 +0530

    ....

--- 17feb.cpp
+++ 17feb.cpp
@@ -56,7 +56,7 @@ int main()
 */
 
 // ex :3   using class object : 
-
+/*
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -98,5 +98,28 @@ int main()
 
     return 0 ;
 }
+*/ 
 
+// pop_back :  remove element  at  end. 
+
+// ex :4 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>v={10,34,56,78,90};
+    
+    v.pop_back(); 
+    v.pop_back(); 
+
+
+    cout<<"vector after pop_back is : "<<endl;
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<v[i]<<endl;
+    }
+    return 0; 
+}
 
