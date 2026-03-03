276f74ca9a5028370782e60937491f13c9b6bbc3
dishant1123 <dishantshah1417@gmail.com>
2026-03-03 12:24:10 +0530

    ....

---
+++ 3march.cpp
@@ -0,0 +1,91 @@
+/*
+exceptional handling  : 
+
+ ==> exceptional handling in cpp is mechnisam to handle runtime errors so that the program does not terminate unexpectedly.
+
+ try ==>block where risky code is written 
+ throw ==>used to throw an exception
+ catch ==>handle the exception
+
+syntax : 
+
+try 
+{
+    // risky code  
+    throw exception_name;
+}
+catch(type_variable)
+{
+    handling code
+}
+
+*/
+
+// ex :1 
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int  a,b; 
+    cout<<"enter the  two numbers : \n"; 
+    cin>>a>>b;
+
+    try 
+    {
+        if(b==0)
+        {
+            throw "division by zero is not allowed. ";
+        }
+        cout<<"the result is : "<<a/b<<endl;
+    }
+    catch(const char* s)
+    {
+        cout<<"exception caught :"<<s<<endl;
+    }
+
+    cout<<"program continues"<<endl;
+    return 0; 
+}
+*/
+
+// ex :2  multiple catch blocks 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int choice; 
+    cout<<"enter 1 or 2 : \n";
+    cin>>choice;
+
+    try 
+    {
+        if(choice==1)
+        {
+            throw 100 ; // throwing an integer exception
+        }
+        else if(choice==2)
+        {
+            throw 3.14 ; // throwing a float exception
+        }
+        else 
+        {
+            throw "invalid choice";
+        }
+    }
+    catch(int x)
+    {
+        cout<<"exception caught : integer exception"<<endl;
+    }
+    catch(float x)
+    {
+        cout<<"exception caught : float exception"<<endl;
+    }
+    catch(const char* s)
+    {
+        cout<<"exception caught  string: "<<s<<endl;
+    }
+    return 0; 
+}
