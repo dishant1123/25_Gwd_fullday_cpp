907b2a716c986ce0c9e9c885ef385e361012a3c7
dishant1123 <dishantshah1417@gmail.com>
2026-03-19 09:51:53 +0530

    ....

--- 25_gwd_cpp/19march.cpp
+++ 25_gwd_cpp/19march.cpp
@@ -7,7 +7,7 @@ array  sort  :
 */
 
 // ex :1 
-
+/*
 #include<iostream>
 using namespace std;
 int  main()
@@ -45,4 +45,50 @@ int  main()
     }
     return 0; 
 
+}
+*/ 
+// ex :2  selection sort : 
+
+/*
+1. finding the min element from unsorted array . 
+2. swapping it with the current position. 
+*/
+
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int a[100],n,i,temp;
+    cout<<"enter the size  of array  : "; 
+    cin>>n; 
+    cout<<"enter the elements  : \n";
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];  // 
+    }
+    cout<<"array  before sorting  : \n";
+    for(i=0; i<n; i++)
+    {
+        cout<<a[i]<<" "; // a[5] = {1,5,2,7,3} 
+    }
+    for(i=0; i<n; i++) // i =1  1 <5 
+    {
+        int minindex =i;  //  minindex =1  
+        for(int j=i+1; j<n; j++) // j=4  4 <5 
+        {
+            if(a[j] < a[minindex]) // a[4] < a[2]  3  < 2  
+            {
+                minindex = j;  //    minindex =2
+            }
+        }
+        temp =a[i];  // temp = 5
+        a[i] = a[minindex]; // a[1] =2
+        a[minindex] = temp; //  a[2] =5    //a[5] = {1,2,5,7,3}
+    }
+    cout<<"array  after sorting  : \n";
+    for(i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0; 
 }
