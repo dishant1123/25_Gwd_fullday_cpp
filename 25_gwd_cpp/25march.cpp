49db70d06f601164ec1d59a007e282b8b1f7623b
dishant1123 <dishantshah1417@gmail.com>
2026-03-25 11:45:03 +0530

    ....

--- 25_gwd_cpp/25march.cpp
+++ 25_gwd_cpp/25march.cpp
@@ -11,6 +11,7 @@ struct student
 
 // ex :1 : 
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -40,5 +41,67 @@ int   main()
     }
     return 0 ; 
 }
-
+*/ 
 // ex : 2 age  > 18  name print  
+
+// ex :3 structure  with  function  with array : 
+
+#include <iostream>
+using namespace std;
+struct student
+{
+    int  id; 
+    string  name;
+    int marks;
+};
+
+void input(student s[],int n)
+{
+    for(int  i=0; i<n; i++)
+    {
+        cout<<"enter id : "<<endl;
+        cin>>s[i].id;
+        cout<<"enter name : "<<endl;
+        cin>>s[i].name;
+        cout<<"enter age : "<<endl;
+        cin>>s[i].marks;
+
+    }
+}
+void display(student s[],int n)
+{
+    cout<<"rollno\tname\tage"<<endl;
+    for(int  i=0; i<n; i++)
+    {
+        cout<<s[i].id<<"\t"<<s[i].name<<"\t"<<s[i].marks<<endl;
+    }
+}
+void highestmarks(student s[],int n)
+{
+    float max = s[0].marks; 
+    int index =0 ; 
+    for(int i=1; i<n; i++)
+    {
+        if(s[i].marks >max) 
+        {
+            max=s[i].marks; 
+            index =i; 
+        }   
+    }
+    cout<<"Topper : \n"; 
+    cout<<"id : "<<s[index].id<<endl;
+    cout<<"name : "<<s[index].name<<endl;
+    cout<<"marks : "<<s[index].marks<<endl;
+}
+int main()
+{
+    int n ; 
+    cout<<"enter no of student : "<<endl;
+    cin>>n;
+
+    student s[n]; 
+    input(s,n);
+    display(s,n);
+    highestmarks(s,n);
+    return 0; 
+}
